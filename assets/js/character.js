2d27f776aeb32117c4cfa93dab31b89a7feee416
Michael <marino.michael.1990@gmail.com>
2020-06-15 15:54:55 +0200

    start

---
+++ assets/js/character.js
@@ -0,0 +1,21 @@
+//Use this script to generate your character
+function Person(race,item){
+    this.race = race;
+    this.item = item;
+    this.currenthealth = 100;
+    this.maxHealth = 100;
+    
+    this.min = 3;
+    this.maxDamage = 20;
+    this.maxHealing = 30;
+
+    this.heal = function(){};
+
+    this.damage = function(){};
+
+    this.totalDamage = this.damage();
+
+    this.displayChar = function(){
+        return console.log(`I am a ${this.race}, I wield a ${this.item}, my total health point are ${this.maxHealth}`);
+    };
+}
