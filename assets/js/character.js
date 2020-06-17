71233c63c78615d0f69e2cc439fca09df67a62bd
Michael <marino.michael.1990@gmail.com>
2020-06-17 11:31:42 +0200

    end elves

--- assets/js/character.js
+++ assets/js/character.js
@@ -3,7 +3,7 @@ class Character{
 
     //DAMAGE STUFF
     min = 3;
-    maxDamage = 20;
+    maxDamage = 30;
     damageBoost = 0;
     maxDamageTaken = 1;
     damageTotal = 0;
@@ -53,7 +53,7 @@ class Character{
              case 'orc' : this.maxHealth = this.maxHealth * 1.4;
                           this.currenthealth = this.maxHealth;
                  break;
-             case 'elf' : this.reflect = 0.3;
+             case 'elves' : this.reflect = 0.3;
                  break;
              case 'skeleton' : this.lifeSteal = 0.10;
                  break;
@@ -75,7 +75,8 @@ class Character{
         if (this.item == 'sword'){
             damage = Math.round(damage * this.damageBoost);
             this.damageTotal = damage;
-        }else{
+        }
+        else{
             this.damageTotal = Math.round(damage);
         }
         
@@ -90,23 +91,16 @@ class Character{
     }
 }
  let player1 = new Character('orc','sword');
- console.log(player1.maxHealth)
- console.log(player1.damageBoost);
 player1.setObjetBoost();
 player1.setRaceBoost();
-console.log(player1.maxHealth)
- console.log(player1.damageBoost);
  player1.displayChar();
 
-let player2 = new Character('skeleton','cape');
+let player2 = new Character('elves','cape');
 player2.setObjetBoost();
 player2.setRaceBoost();
 player2.displayChar();
 
-
-// let finalDamageP1 = player1.damageTotal * player2.maxDamageTaken;
-
-let finalDamageP2 = Math.round(player2.damageTotal*player1.maxDamageTaken);
+// LES BOUTTONS DE COMBAT 
 
 let attackButtonP1 = document.getElementById("attack1");
 let attackButtonP2 = document.getElementById("attack2");
@@ -120,7 +114,7 @@ let yieldButtonP2 = document.getElementById("surrender2");
 
  // RANDOM POUR SAVOIR QUEL JOUEUR COMMENCE
  let playerTurn = Math.round(Math.random() + 1);
-console.log(playerTurn);
+console.log("le joueur "+playerTurn+" commence");
 
 // MY FUNCTION
 function endTurnP1(){
@@ -191,6 +185,59 @@ function randFromRange(range) {
         (Math.random() * (range[1] - range[0]) + range[0])
     ); //random between min - max included
 }
+function checkEndTurn(playerAttack, playerDefense){
+    if(playerDefense.currenthealth<=0){
+        playerDefense.currenthealth = 0;
+        endGame();
+        alert(playerAttack.race+" WON");
+    }else if(playerAttack.currenthealth <= 0){
+        playerAttack.currenthealth = 0;
+        endGame();
+        alert(playerDefense.race+" WON")
+    }
+    else{
+        endTurnP1();
+    }
+}
+function dodgeBoots(playerAttack,playerDefense){
+    if(playerDefense.item == 'boots'){
+        let randomDodgeBoots = Math.random();
+        if(randomDodgeBoots <= playerDefense.dodgeChance){
+            playerDefense.currenthealth = playerDefense.currenthealth;
+            console.log("DODGE !!! vie restante de "+playerDefense.race+" : "+playerDefense.currenthealth);
+        }else{
+            playerDefense.currenthealth = playerDefense.currenthealth - Math.round(playerAttack.damageTotal * playerDefense.maxDamageTaken);
+            console.log("HIT ! vie restante au "+playerDefense.race+" : "+playerDefense.currenthealth);
+        }
+    }else{
+            playerDefense.currenthealth = playerDefense.currenthealth - Math.round(playerAttack.damageTotal * playerDefense.maxDamageTaken);
+            console.log("HIT ! vie restante au "+playerDefense.race+" : "+playerDefense.currenthealth);
+    }
+}
+function reflectElvesAndBoots(playerAttack,playerDefense){
+    let finalDamage = Math.round(playerAttack.damageTotal*playerDefense.maxDamageTaken);
+    console.log("dégat infligé par "+playerAttack.race+" : "+finalDamage);
+    if(playerDefense.race == 'elves'){
+        let randomReflect = Math.random();
+        if(randomReflect<= playerDefense.reflect){
+            let reflectDamage = finalDamage/2;
+            playerDefense.currenthealth = playerDefense.currenthealth;
+            playerAttack.currenthealth = playerAttack.currenthealth - reflectDamage;
+            console.log("REFLECT !! "+playerAttack.race+" recois "+reflectDamage+" dégats, il lui reste : "+playerAttack.currenthealth);
+            checkEndTurn(playerAttack,playerDefense);
+           
+        }else{
+            console.log("miss the reflect ...");
+            dodgeBoots(playerAttack,playerDefense);
+            checkEndTurn(playerAttack,playerDefense);
+            
+        }
+   
+    }else{
+       dodgeBoots(playerAttack,playerDefense);
+       checkEndTurn(playerAttack,playerDefense);
+    }
+ }
 
 if(playerTurn == 1){
     attackButtonP2.disabled = true;
@@ -208,19 +255,50 @@ else{
 
 document.getElementById("attack1").addEventListener("click", function(){
     player1.damage();
-    console.log(player1.damageTotal);
-    console.log(player2.maxDamageTaken);
-    console.log("dégat infligé par le joueur 1 :"+Math.round(player1.damageTotal * player2.maxDamageTaken));
+
+    // CALCUL DES DEGATS AVEC PRISE EN COMPTE DES LESS DAMAGE DE HUMAN 
+    // let finalDamageP1 = Math.round(player1.damageTotal*player2.maxDamageTaken);
+    // console.log("dégat infligé par "+player1.race+" : "+finalDamageP1);
     
-    player2.currenthealth = player2.currenthealth - Math.round(player1.damageTotal * player2.maxDamageTaken);
-    console.log("vie restante au joueur 2: "+player2.currenthealth);
-    if(player2.currenthealth<=0){
-        player2.currenthealth = 0;
-        endGame();
-        alert("PLAYER 1 WON");
-    }else{
-        endTurnP1();
-    }
+     //CALCUL REFLECT ELVES
+    //  if(player2.race == 'elves'){
+    //      let randomReflect = Math.random();
+    //      if(randomReflect<= player2.reflect){
+    //          let reflectDamage = finalDamageP1/2;
+    //          player2.currenthealth = player2.currenthealth;
+    //          player1.currenthealth = player1.currenthealth - reflectDamage;
+    //          console.log("REFLECT !! "+player1.race+" recois "+reflectDamage+" dégats, il lui reste : "+player1.currenthealth);
+    //          checkEndTurn(player1,player2);
+            
+    //      }else{
+    //          console.log("miss the reflect ...");
+    //          dodgeBoots(player1,player2);
+    //          checkEndTurn(player1,player2);
+             
+    //      }
+    
+    //  }else{
+    //     dodgeBoots(player1,player2);
+    //     checkEndTurn(player1,player2);
+    //  }
+    reflectElvesAndBoots(player1,player2);
+    
+    // CALCUL DE CHANCE DE DODGE AVEC LES BOOTS
+    
+    
+    
+    
+    // CALCUL DE CHANCE DE DOUBLE HIT
+    
+    
+    // VERIF FIN DE TOUR
+    // if(player2.currenthealth<=0){
+    //     player2.currenthealth = 0;
+    //     endGame();
+    //     alert("PLAYER 1 WON");
+    // }else{
+    //     endTurnP1();
+    // }
     
 });
 document.getElementById("heal1").addEventListener("click", function(){
@@ -237,8 +315,9 @@ document.getElementById("surrender1").addEventListener("click", function(){
 
 document.getElementById("attack2").addEventListener("click", function(){
     player2.damage();
+    // CALCUL DES DEGATS AVEC PRISE EN COMPTE DES LESS DAMAGE DE HUMAN 
+    let finalDamageP2 = Math.round(player2.damageTotal*player1.maxDamageTaken);
     console.log("dégat infligé par le joueur 2 :"+finalDamageP2);
-    
     player1.currenthealth = player1.currenthealth - finalDamageP2;
     console.log("vie restante au joueur 1: "+player1.currenthealth);
     if(player1.currenthealth<=0){
